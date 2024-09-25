ec63e896f8ef4e171a2b377ea3f77cd81449fc77
admin <tvb5449@gmail.com>
2024-09-25 14:24:30 +0900

    um jun sik

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,11 +3,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController{
-    @GetMapping("/hello") // 전송 방식 GET
-    public String hello(Model model) {
+    @GetMapping("/about_detailed") // 전송 방식 GET
+    public String about_detailed(Model model) {
         model.addAttribute("data","홈페이지 메인"); // model 설정
-        return "hello"; // hello.html 연결
+        return "about_detailed"; // hello.html 연결
     }
 }
