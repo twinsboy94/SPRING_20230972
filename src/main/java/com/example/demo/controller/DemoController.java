69adaad7e285d32c324c5f1ec8a1f555c6558ea1
admin <tvb5449@gmail.com>
2024-10-16 10:00:41 +0900

    moongtangi

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.service.TestService;
+
+@Controller // 컨트롤러 어노테이션 명시
+public class DemoController{
+    @Autowired
+    TestService testService;
+    
+    @GetMapping("/testdb")
+        public String getAllTestDBs(Model model) {
+        TestDB test = testService.findByName("홍길동");
+        model.addAttribute("data4", test);
+        System.out.println("데이터 출력 디버그 : " + test);
+        return "testdb";
+    }
+}
